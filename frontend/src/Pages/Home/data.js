283fdd0d852d90a6e36ad35d06055e58ada947f2
saikhmirsat <saikh.mirsat.786@gmail.com>
2023-03-24 14:40:57 +0530

    add

---
+++ frontend/src/Pages/Home/data.js
@@ -0,0 +1,115 @@
+export const menData = [
+    {
+        "image": "https://static01.manyavar.com/uploads/dealimages/11305/detailimages/robust-red-indo-western-wear-cpiw509-307-8.jpg",
+        "ProductViewClick href": "https://www.manyavar.com/striking-maroon-indo-western-cpiw509-307",
+        "title": "Striking Maroon Indo-Western",
+        "price": "₹ 15999"
+    },
+    {
+        "image": "https://static01.manyavar.com/uploads/dealimages/10618/detailimages/bright-maroon-patternd-kurta-set-sosk371-307-4.jpg",
+        "ProductViewClick href": "https://www.manyavar.com/bright-maroon-patterned-kurta-set-sosk371-307",
+        "title": "Bright Maroon Patterned Kurta Set",
+        "price": "₹ 3999"
+    },
+    {
+        "image": "https://static01.manyavar.com/uploads/dealimages/13877/detailimages/summer-essential-blue-kurta-ml12032-306-4.jpg",
+        "ProductViewClick href": "https://www.manyavar.com/sapphire-blue-printed-kurta-ml12032-306",
+        "title": "Sapphire Blue Printed Kurta",
+        "price": "₹ 2499"
+    },
+    {
+        "image": "https://static01.manyavar.com/uploads/dealimages/14682/detailimages/ML12098-308_01.JPG",
+        "ProductViewClick href": "https://www.manyavar.com/Grey-Bandhani-Print-Kurta-ML12098-308",
+        "title": "Grey Bandhani Print Kurta",
+        "price": "₹ 2499"
+    },
+    {
+        "image": "https://static01.manyavar.com/uploads/dealimages/14505/detailimages/sosk605-325_01.jpg",
+        "ProductViewClick href": "https://www.manyavar.com/salmon-pink-jacquard-kurta-pajama-sosk605-325",
+        "title": "Salmon Pink Jacquard Kurta Pajama",
+        "price": "₹ 4999"
+    },
+    {
+        "image": "https://static01.manyavar.com/uploads/dealimages/14350/detailimages/sdes607-327_01.jpg",
+        "ProductViewClick href": "https://www.manyavar.com/dapper-light-green-kurta-set-sdes607-327",
+        "title": "Dapper Light Green Kurta Set",
+        "price": "₹ 3999"
+    },
+    {
+        "image": "https://static01.manyavar.com/uploads/dealimages/14513/detailimages/CPOSK504-322_01.JPG",
+        "ProductViewClick href": "https://www.manyavar.com/wine-floral-print-kurta-set-cposk504-322",
+        "title": "Wine Floral Print Kurta Set",
+        "price": "₹ 3499"
+    },
+    {
+        "image": "https://static01.manyavar.com/uploads/dealimages/14438/detailimages/ides738-333_01.jpg",
+        "ProductViewClick href": "https://www.manyavar.com/sage-green-self-design-indo-western-ides738-333",
+        "title": "Sage Green Self Design Indo Western",
+        "price": "₹ 17999"
+    },
+    {
+        "image": "https://static01.manyavar.com/uploads/dealimages/14321/detailimages/o951d504-333_01.jpg",
+        "ProductViewClick href": "https://www.manyavar.com/opal-green-sherwani-o951d504-333",
+        "title": "Opal Green Sherwani",
+        "price": "₹ 20999"
+    },
+    {
+        "image": "https://static01.manyavar.com/uploads/dealimages/13991/detailimages/cpsh144-340_01.jpg",
+        "ProductViewClick href": "https://www.manyavar.com/enchanting-cream-sherwani-cpsh144-340",
+        "title": "Enchanting Cream Sherwani",
+        "price": "₹ 14999"
+    }
+]
+
+export const womenData = [
+    {
+        "image": "https://static01.manyavar.com/uploads/dealimages/13931/detailimages/regal-rani-bridal-lehenga-unlb4269v-425-2.jpg",
+        "title": "Regal Rani Bridal Lehenga",
+        "price": "₹ 29999"
+    },
+    {
+        "image": "https://static01.manyavar.com/uploads/dealimages/14057/detailimages/ethereal-rani-pink-lehenga-unlb4292-425-12.jpg",
+        "title": "Ethereal Rani Pink Lehenga",
+        "price": "₹ 29999"
+    },
+    {
+        "image": "https://static01.manyavar.com/uploads/dealimages/14184/detailimages/ravishing-rani-pink-lehenga-unlb4268-425-7.jpg",
+        "title": "Ravishing Rani Pink Lehenga",
+        "price": "₹ 34999"
+    },
+    {
+        "image": "https://static01.manyavar.com/uploads/dealimages/14239/detailimages/unlb4239-403_01.jpg",
+        "title": "Classy Cream Georgette Lehenga",
+        "price": "₹ 19999"
+    },
+    {
+        "image": "https://static01.manyavar.com/uploads/dealimages/14451/detailimages/ulb4367v-416_01.jpg",
+        "title": "Ruby Red Velvet Lehenga",
+        "price": "₹ 29999"
+    },
+    {
+        "image": "https://static01.manyavar.com/uploads/dealimages/14169/detailimages/sea-green-velvet-lehenga-unlb4279v-436-1.jpg",
+        "title": "Sea Green Velvet Lehenga",
+        "price": "₹ 19999"
+    },
+    {
+        "image": "https://static01.manyavar.com/uploads/dealimages/14172/detailimages/georgette-blue-lehenga-unlb4280-426-2.jpg",
+        "title": "Georgette Blue Lehenga",
+        "price": "₹ 19999"
+    },
+    {
+        "image": "https://static01.manyavar.com/uploads/dealimages/14179/detailimages/unlb4326-412_01.jpg",
+        "title": "Elegant Peach Lehenga",
+        "price": "₹ 20999"
+    },
+    {
+        "image": "https://static01.manyavar.com/uploads/dealimages/14173/detailimages/unlb4282-422_01.jpg",
+        "title": "Chiffon Wine Red Lehenga",
+        "price": "₹ 19999"
+    },
+    {
+        "image": "https://static01.manyavar.com/uploads/dealimages/14189/detailimages/ulb4366-411_01.jpg",
+        "title": "Charming Light Green Lehenga",
+        "price": "₹ 29999"
+    }
+]
