e0ab055f1f37f29a0f1c9ecdb032ee2bc5c3a302
saikhmirsat <saikh.mirsat.786@gmail.com>
2023-04-17 15:12:23 +0530

    done product page

--- frontend/src/Pages/Products/Card.jsx
+++ frontend/src/Pages/Products/Card.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import { useState } from 'react'
 import styles from './Products.module.css'
 
+import { AiOutlineHeart } from 'react-icons/ai'
+import { AiTwotoneHeart } from 'react-icons/ai'
+
 export default function Card({ image1, image2 }) {
     const [hover, setHover] = useState(false)
     // console.log(hover)
+
+    const [wish, setWish] = useState(true)
+    // console.log(wish)
 
     const hoverIMG = () => {
         setHover(true)
@@ -18,7 +24,8 @@ export default function Card({ image1, image2 }) {
         //     <img src={hover ? image2 : image1} alt="" onMouseEnter={hoverIMG} onMouseLeave={normalIMG} />
         // </div>
         <div className={styles.HomeCardImg_Div}>
-            <img src={hover ? image2 : image1} alt="" onMouseEnter={hoverIMG} onMouseLeave={normalIMG} />
+            <div onClick={() => setWish(!wish)} className={styles.wishlistHeart}>{wish ? <AiOutlineHeart color='white' size='25px' /> : <AiTwotoneHeart size='25px' color='red' />}</div>
+            <img style={{ transition: '0.1s' }} src={hover ? image2 : image1} alt="" onMouseEnter={hoverIMG} onMouseLeave={normalIMG} />
         </div>
     )
 }
